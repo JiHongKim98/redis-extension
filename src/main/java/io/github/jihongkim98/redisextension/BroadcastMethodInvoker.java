e7a540510aaf37c60cfc1917744879cad1af1343
JiHongKim98 <kinjihong9598@gmail.com>
2024-11-29 23:01:10 +0900

    feat: Add broadcast method invoker and container registry

---
+++ src/main/java/io/github/jihongkim98/redisextension/BroadcastMethodInvoker.java
@@ -0,0 +1,37 @@
+package io.github.jihongkim98.redisextension;
+
+import java.lang.reflect.Method;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.core.log.LogAccessor;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+public class BroadcastMethodInvoker implements MessageListener {
+
+    private static final RedisSerializer<String> serializer = RedisSerializer.string();
+
+    private final LogAccessor logger = new LogAccessor(LogFactory.getLog(getClass()));
+
+    private final Object delegate;
+
+    private final Method method;
+
+    public BroadcastMethodInvoker(Object delegate, Method method) {
+        this.delegate = delegate;
+        this.method = method;
+    }
+
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+        byte[] body = message.getBody();
+        String payload = serializer.deserialize(body);
+
+        try {
+            method.invoke(delegate, payload);
+        } catch (Throwable th) {
+            logger.error(th, "Failed to invoke method '" + method.getName() +
+                    "' on delegate '" + delegate.getClass().getName() + "'");
+        }
+    }
+}
--- src/main/java/io/github/jihongkim98/redisextension/TopicRegistrar.java
+++ src/main/java/io/github/jihongkim98/redisextension/TopicRegistrar.java
@@ -1,21 +1,31 @@
 package io.github.jihongkim98.redisextension;
 
 import java.lang.reflect.Method;
-import org.apache.commons.logging.Log;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.SmartInitializingSingleton;
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.log.LogAccessor;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.Topic;
 
 public class TopicRegistrar implements SmartInitializingSingleton {
 
-    private final Log logger = LogFactory.getLog(TopicRegistrar.class);
+    private final LogAccessor logger = new LogAccessor(LogFactory.getLog(getClass()));
+
+    private final Map<BroadcastMethodInvoker, List<Topic>> invokers = new ConcurrentHashMap<>();
 
     private ApplicationContext applicationContext;
 
-    public void registry(Object delegate, Method method, Topic topic) {
-        // TODO
+    private RedisMessageListenerContainer container;
 
+    public void registry(Object delegate, Method method, Topic topic) {
+        BroadcastMethodInvoker invoker = new BroadcastMethodInvoker(delegate, method);
+        invokers.computeIfAbsent(invoker, v -> new ArrayList<>()).add(topic);
     }
 
     public void setApplicationContext(ApplicationContext applicationContext) {
@@ -24,7 +34,20 @@ public class TopicRegistrar implements SmartInitializingSingleton {
 
     @Override
     public void afterSingletonsInstantiated() {
-        // TODO
+        try {
+            this.container = applicationContext.getBean(RedisMessageListenerContainer.class);
+        } catch (BeansException e) {
+            logger.error("RedisMessageListenerContainer bean is missing in the application context. " +
+                    "Please register it as a @Bean in your configuration class to enable Redis Pub/Sub functionality.");
+            throw e;
+        }
+        containerRegistry();
+        this.container.start();
+    }
 
+    private void containerRegistry() {
+        invokers.forEach((invoker, topics) -> {
+            this.container.addMessageListener(invoker, topics);
+        });
     }
 }
