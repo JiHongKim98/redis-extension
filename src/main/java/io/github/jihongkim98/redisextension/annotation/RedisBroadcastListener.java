b0deb4efdbea4d1dddf88443dec3341ff417a55c
JiHongKim98 <kinjihong9598@gmail.com>
2024-11-29 22:57:18 +0900

    refactor: Add default channels value

--- src/main/java/io/github/jihongkim98/redisextension/annotation/RedisBroadcastListener.java
+++ src/main/java/io/github/jihongkim98/redisextension/annotation/RedisBroadcastListener.java
@@ -9,7 +9,7 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface RedisBroadcastListener {
 
-    String[] channels();
+    String[] channels() default {};
 
     String[] patterns() default {};
 }
