133e4bdcc4dad56c4621e970b252dedd9df62d24
JiHongKim98 <kinjihong9598@gmail.com>
2024-12-05 11:31:25 +0900

    fix: Fixed bean name in annotated bean processor class

--- src/main/java/io/github/jihongkim98/redisextensions/BroadcastConfigUtils.java
+++ src/main/java/io/github/jihongkim98/redisextensions/BroadcastConfigUtils.java
@@ -3,7 +3,7 @@ package io.github.jihongkim98.redisextensions;
 public class BroadcastConfigUtils {
 
     public static final String REDIS_BROADCAST_ANNOTATION_BEAN_POST_PROCESSOR =
-            "io.github.jihongkim98.redisextension.RedisBroadcastAnnotationBeanPostProcessor";
+            "io.github.jihongkim98.redisextensions.RedisBroadcastAnnotationBeanPostProcessor";
 
     private BroadcastConfigUtils() {
     }
